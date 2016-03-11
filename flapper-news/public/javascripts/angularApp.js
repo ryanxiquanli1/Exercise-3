5a67e12403245e1fa88a9024f548fe837babb100
ryanxiquanli1 <xli1@mail.sfsu.edu>
2016-03-11 10:59:57 +0000

    part3 completed

--- flapper-news/public/javascripts/angularApp.js
+++ flapper-news/public/javascripts/angularApp.js
@@ -9,8 +9,14 @@ function($stateProvider, $urlRouterProvider) {
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
-      controller: 'MainCtrl'
-    })
+      controller: 'MainCtrl',
+      resolve: {
+      postPromise: ['posts', function(posts){
+      return posts.getAll();
+    }]
+  }
+})
+
          .state('posts', {
   url: '/posts/{id}',
   templateUrl: '/posts.html',
@@ -20,10 +26,23 @@ function($stateProvider, $urlRouterProvider) {
   $urlRouterProvider.otherwise('home');
 }]);
 
-app.factory('posts', [function(){
+app.factory('posts', ['$http', function($http){
   var o = {
     posts: []
   };
+   o.getAll = function() {
+    return $http.get('/posts').success(function(data){
+      angular.copy(data, o.posts);
+    });
+  };
+  
+  o.create = function(post) {
+  return $http.post('/posts', post).success(function(data){
+    o.posts.push(data);
+  });
+};
+
+
   return o;
 }]);
 
@@ -37,14 +56,10 @@ function($scope,posts) {
   
   $scope.addPost = function(){
   if(!$scope.title || $scope.title === '') { return; }
-  $scope.posts.push({
+  post.create({
     title: $scope.title,
     link: $scope.link,
-    upvotes: 0,
-      comments: [
-    {author: 'Joe', body: 'Cool post!', upvotes: 0},
-    {author: 'Bob', body: 'Great idea but everything is wrong!', upvotes: 0}
-  ]
+    
 });
 
   $scope.title = '';
